e5019a69dd964f6733c128d00c018e1c0b1c658e
hibaanj <anjumhiba15@gmail.com>
2022-07-09 22:59:34 +0530

    Add project files.

---
+++ day3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace day3
+{
+    public class day3
+    {
+        public static void Main(string[] args)
+        {
+            Uc1emppresent uc1 = new Uc1emppresent();
+            uc1.Emppresent();
+
+            Uc2empwage uc2 = new Uc2empwage();
+            uc2.Empwage();
+
+
+
+        }
+    }
+}
---
+++ day3/Uc2empwage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day3
+{
+    internal class Uc2empwage
+    {
+            public void Empwage()
+            {
+                int FULLTIME = 1;
+                int rateperhour = 15;
+                //calculating 
+                int empwage = 0;
+                int emphr = 0;
+                Random random = new Random();
+                int check = random.Next(0, 2);
+                if (check == FULLTIME)
+                {
+                    Console.WriteLine("emp is present today");
+                    emphr = 8;
+                }
+                else
+                {
+                    Console.WriteLine("emp is absent today");
+                    emphr = 0;
+                }
+                //wage calculation
+                empwage = emphr * rateperhour;
+                Console.WriteLine("todays wage is" + empwage);
+
+            }
+        
+    }
+}
