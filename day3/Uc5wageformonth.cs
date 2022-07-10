b6a0ebf89933209ee07e07daea1e19d5b2005de5
hibaanj <anjumhiba15@gmail.com>
2022-07-10 21:19:51 +0530

    uc5 wage for a month

---
+++ day3/Uc5wageformonth.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day3
+{
+    internal class Uc5wageformonth
+    {
+        public void Wageforamonth()
+        {
+            const int FULLTIME = 1;
+            const int PARTTIME = 2;
+            const int WAGEPERHOUR = 20;
+            const int WORKINGDAYS = 20;
+            int wagemonth = 0;
+            for (int day = 0; day < WORKINGDAYS; day++)
+            {
+                int emphr = 0;
+                int wage = 0;
+                Random random = new Random();
+                int check = random.Next(0, 3);
+                switch (check)
+                {
+                    case FULLTIME:
+                        emphr = 12;
+                        break;
+                    case PARTTIME:
+                        emphr = 8;
+                        break;
+                    default:
+                        emphr = 0;
+                        break;
+
+                }
+                wage = emphr * WAGEPERHOUR;
+                wagemonth += wage;
+            }
+            Console.WriteLine("wage for a month is " + wagemonth);
+        }
+    }
+}
